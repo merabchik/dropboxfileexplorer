e089a8b3cdf825767d149ee370b2ece2a87ee1ee
merabchik83@gmail.com <merabchik>
2019-10-17 14:52:45 +0400

    Add isomorphic-fetch lib

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const dropbox = require('dropbox');
+require('isomorphic-fetch'); // or another library of choice.
+var Dropbox = require('dropbox').Dropbox;
 const bodyParser = require("body-parser");
 
 let app = new express();
-
+let port = 8181;
 
 
 
 app.use(bodyParser.json());
 
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on port ` + port);
+});
+
 app.get("/", function (req, resp) {
 
-    let dbx = new dropbox.Dropbox({ accessToken: '******', fetch: fetch });
+    let dbx = new Dropbox({ accessToken: '**********'});
     dbx.filesListFolder({ path: '' })
         .then(function (response) {
             console.log(response);
@@ -20,15 +26,4 @@ app.get("/", function (req, resp) {
             console.log(error);
         });
     resp.send("<h1>Welcome to NodeJs server app</h1>");
-});
-
-app.get("/rest/apps/list/:id", (req, resp) => {
-    resp.send('id: ' + req.params.id);
-});
-
-
-
-// Start server
-app.listen('localhost', 3000, () => {
-    console.log(`Server running at http://127.0.0.1:8080/`);
 });
